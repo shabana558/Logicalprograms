8699f3aff4fdf439ece35208590f09ab0bd8a950
shaik ameer basha <shaikshabana758@gmail.com>
2022-01-27 17:01:12 +0530

    [shabana] adding Logicalprograms

---
+++ Logicalprograms/Logicalprograms/Fibonocciseries.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logicalprograms
+{
+    internal class Fibonocciseries
+    {
+        public void FindFibonocciSeries()
+        {
+            int val1 = 0, val2 = 1, val3, i, n;
+            n = 7;
+            Console.WriteLine(" Fibonacci series:");
+            Console.Write(val1 + " " + val2 + " ");
+            for (i = 2; i < n; ++i)
+            {
+                val3 = val1 + val2;
+                Console.Write(val3 + " ");
+                val1 = val2;
+                val2 = val3;
+            }
+        }
+    }
+
+}
